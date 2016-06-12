216a185f71f5d22430b8c5a6b2b23847334802eb
Simon Morley <simon@polkaspots.com>
2016-06-12 17:30:34 +0100

    Sorting the developer section

--- client/components/apps/apps.directives.js
+++ client/components/apps/apps.directives.js
@@ -2,9 +2,12 @@
 
 var app = angular.module('myApp.apps.directives', []);
 
-app.directive('listApps', ['App', '$routeParams', function(App, $routeParams) {
+app.directive('listApps', ['App', '$routeParams', 'menu', function(App, $routeParams, menu) {
 
   var link = function(scope) {
+
+    menu.isOpen = false;
+    menu.hideBurger = true;
 
     scope.loading   = true;
 
@@ -13,7 +16,6 @@ app.directive('listApps', ['App', '$routeParams', function(App, $routeParams) {
         scope.apps        = results;
         scope.loading     = undefined;
         scope.predicate   = '-created_at';
-        // scope._links      = results._links;
       }, function(err) {
         scope.loading = undefined;
       });
@@ -28,34 +30,17 @@ app.directive('listApps', ['App', '$routeParams', function(App, $routeParams) {
     link: link,
     scope: {
     },
-    template:
-      '<h1>Cucumber Apps</h1>' +
-      '<p>Integration your favourite services with Tony or make your own application.</p>'+
-      '<a href=\'/#/apps/new\' class=\'button\'>New Application</a>'+
-      '<hr>'+
-      '<div class=\'row apps\' ng-repeat=\'app in apps\'>' +
-      '<div class=\'small-12 columns\'>' +
-      '<div class=\'row\'>' +
-      '<div class=\'small-12 medium-1 columns\'>' +
-      '<i class="fa fa-rocket fa-3x app-icon"></i>'+
-      // '<img src=\'{{ app.icon || "https://abs.twimg.com/a/1404172626/images/oauth_application.png" }}\'>' +
-      '</div>'+
-      '<div class=\'small-12 medium-11 columns\'>' +
-      '<h3><a href=\'/#/apps/{{ app.id }}\'>{{ app.name }}</a></h3>' +
-      '<p>{{ app.description }}</p>'+
-      '<p>{{ app.created_at | humanTime }}</p>'+
-      '</div>'+
-      '</div>'+
-      '</div>'+
-      '</div>'
-
+    templateUrl: 'components/apps/_index.html'
   };
 
 }]);
 
-app.directive('showApp', ['App', '$routeParams', function(App, $routeParams) {
+app.directive('showApp', ['App', '$routeParams', 'menu', function(App, $routeParams, menu) {
 
   var link = function(scope) {
+
+    menu.isOpen = false;
+    menu.hideBurger = true;
 
     scope.loading   = true;
     scope.appId     = '●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●';
@@ -143,13 +128,16 @@ app.directive('showApp', ['App', '$routeParams', function(App, $routeParams) {
 
 }]);
 
-app.directive('editApp', ['App', '$routeParams', '$location', function(App, $routeParams, $location) {
+app.directive('editApp', ['App', '$routeParams', '$location', 'menu', 'showErrors', function(App, $routeParams, $location, menu, showErrors) {
 
   var link = function(scope) {
 
+    menu.isOpen = false;
+    menu.hideBurger = true;
+
     scope.loading         = true;
 
-    scope.init = function() {
+    var init = function() {
       App.query({id: $routeParams.id}).$promise.then(function(results) {
         scope.app        = results;
         scope.loading     = undefined;
@@ -158,17 +146,15 @@ app.directive('editApp', ['App', '$routeParams', '$location', function(App, $rou
       });
     };
 
-    scope.create = function() {
-      // App.create({app: scope.app}).$promise.then(function(results) {
-      //   $location.path('/apps/' + results.id).search({n: true});
-      // }, function(err) {
-      //   if ( err.data && err.data.message ) {
-      //     formatErrors(err.data.message);
-      //   }
-      // });
+    var create = function() {
+      App.create({app: scope.app}).$promise.then(function(results) {
+        $location.path('/apps/' + results.id).search({n: true});
+      }, function(err) {
+        showErrors(err);
+      });
     };
 
-    scope.update = function() {
+    var update = function() {
       // App.update({id: $routeParams.id, app: scope.app}).$promise.then(function(results) {
       //   $location.path('/apps/' + results.id).search({n: true});
       // }, function(err) {
@@ -180,16 +166,14 @@ app.directive('editApp', ['App', '$routeParams', '$location', function(App, $rou
 
     scope.save = function() {
       if (scope.app && scope.app.id) {
-        scope.update();
+        update();
       } else {
-        scope.create();
+        create();
       }
     };
-
-    function formatErrors(res) {
-    }
+    
     if ($routeParams.id) {
-      scope.init();
+      init();
     }
 
   };
@@ -198,45 +182,7 @@ app.directive('editApp', ['App', '$routeParams', '$location', function(App, $rou
     link: link,
     scope: {
     },
-    template:
-      '<h1>{{ scope.app.id ? "Edit" : "Create" }} App</h1>' +
-      '<p>This is for developers. If you\'re not sure what an API is, it\'s best you forget this exists. <br>Developer documentation can be <a href=\'http://docs.polkaspots.apiary.io/#\'>found here</a>.</p>'+
-      '<div>' +
-      '<form name=\'myForm\' ng-submit=\'save()\'>' +
-      '<div class=\'row\'>'+
-      '<div class=\'small-12 medium-6 columns\'>'+
-      '<label for=\'name\'>Application Name</label>'+
-      '<input type=\'text\' ng-model=\'app.name\' ng-class="{ \'input-error\' : myForm.name.$error.required || myForm.name.$error.maxlength }" name=\'name\' placeholder=\'Your application name\' ng-maxlength=32 ng-minlength=5 required>'+
-      '<p ng-show=\'myForm.name.$error.maxlength || myForm.name.$error.minlength\'><small>Min 5 and max 32 characters.</small></p>'+
-      '<p ng-hide=\'myForm.name.$error.maxlength\' class=\'text-muted\'><small>This will be used on any user-facing authorisation screens. Max 32 characters.</small></p>'+
-      '</div>'+
-      '</div>'+
-      '<div class=\'row\'>'+
-      '<div class=\'small-12 medium-6 columns\'>'+
-      '<label for=\'name\'>Callback URL</label>'+
-      '<input type=\'url\' ng-model=\'app.redirect_uri\' ng-class="{ \'input-error\' : myForm.redirect_uri.$error.required || myForm.redirect_uri.$error.url }" name=\'redirect_uri\' placeholder=\'Your callback URL\' required>'+
-      '<p ng-hide=\'myForm.name.$error.maxlength\' class=\'text-muted\'><small>Where should we return to after a successful authentication. Must be https.</small></p>'+
-      '</div>'+
-      '</div>'+
-      '<div class=\'row\'>'+
-      '<div class=\'small-12 medium-6 columns\'>'+
-      '<label for=\'name\'>Application Website</label>'+
-      '<input type=\'url\' ng-model=\'app.website\' ng-class="{ \'input-error\' : myForm.website.$error.required || myForm.website.$error.url }" name=\'website\' placeholder=\'Your website\' required>'+
-      '<p class=\'text-muted\'><small>Your publicly available website where your users will go to find out about your application.</small></p>'+
-      '</div>'+
-      '</div>'+
-      '<div class=\'row\'>'+
-      '<div class=\'small-12 medium-6 columns\'>'+
-      '<label for=\'name\'>Description</label>'+
-      '<textarea ng-model=\'app.description\' ng-class="{ \'input-error\' : myForm.description.$error.required }" name=\'description\' placeholder=\'Tell us about your App\' rows=5 required></textarea>'+
-      '<p class=\'text-muted\'><small>This won\'t be available to your users, it\'s just so we know what you\'re building</small></p>'+
-      '</div>'+
-      '</div>'+
-      '<button ng-disabled="myForm.$invalid || myForm.$pristine" class="button" id="update">{{ app.id ? "Update" : "Create" }} <span ng-show="creating"> <i class="fa fa-cog fa-spin"></i></span> <span ng-show="created"> <i class="fa fa-check"></i></span><span ng-show="errors"> <i class="fa fa-frown"></i></span></button>'+
-      '</form>'+
-      '<p>By creating this application, you are agreeing to the developer agreement found <a href="https://cucumberwifi.io/developer-terms" target="_blank">here</a>.</p>'+
-      '</div>'
-
+    templateUrl: 'components/apps/_new.html'
   };
 
 }]);
