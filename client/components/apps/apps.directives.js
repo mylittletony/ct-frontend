988fddafe8986a0092a8a11555305222fc5d8f97
Simon Morley <simon@polkaspots.com>
2016-06-13 10:12:39 +0100

    Formats developer section for new interface fixes #23

--- client/components/apps/apps.directives.js
+++ client/components/apps/apps.directives.js
@@ -43,8 +43,8 @@ app.directive('showApp', ['App', '$routeParams', 'menu', function(App, $routePar
     menu.hideBurger = true;
 
     scope.loading   = true;
-    scope.appId     = '●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●';
-    scope.appSecret = '●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●';
+    // scope.appId     = '●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●';
+    // scope.appSecret = '●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●';
 
     scope.init = function() {
       App.query({id: $routeParams.id}).$promise.then(function(results) {
@@ -63,16 +63,15 @@ app.directive('showApp', ['App', '$routeParams', 'menu', function(App, $routePar
       }
     };
 
-    scope.showAppId = function() {
-      scope.showId = !scope.showId;
-      scope.appId = scope.showId ? scope.app.uid : '●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●';
-    };
+    // scope.showAppId = function() {
+    //   scope.showId = !scope.showId;
+    //   scope.appId = scope.showId ? scope.app.uid : '●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●';
+    // };
 
-    scope.showAppSecret = function() {
-      scope.showSecret = !scope.showSecret;
-      scope.appSecret = scope.showSecret ? scope.app.secret : '●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●';
-    };
-
+    // scope.showAppSecret = function() {
+    //   scope.showSecret = !scope.showSecret;
+    //   scope.appSecret = scope.showSecret ? scope.app.secret : '●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●';
+    // };
 
     scope.init();
 
@@ -80,55 +79,13 @@ app.directive('showApp', ['App', '$routeParams', 'menu', function(App, $routePar
 
   return {
     link: link,
-    scope: {
-    },
-    template:
-      '<h1>{{ app.name }}</h1>'+
-      '<h4>{{ app.description || "No description" }}</h4>'+
-      '<hr>'+
-      '<h3>Scopes</h3>'+
-      '<p>{{ app.scopes.length ? app.scopes : \'None\' }}</p>'+
-      '<hr>'+
-      '<ul class="no-bullet">'+
-      '<h3>Callback URLs:</h3>'+
-      '<ul ng-repeat=\'a in uris\'>'+
-      '<li ng-if=\'a\'>{{ a }}</li>'+
-      '</ul>'+
-      '<hr>'+
-      '<h3>Application URLs:</h3>'+
-      '<ul>'+
-      '<li>https://id.ctapp.io/oauth/authorize</li>'+
-      '<li>https://id.ctapp.io/oauth/token</li>'+
-      '<li>https://id.ctapp.io/oauth/revoke</li>'+
-      '</ul>'+
-      '<hr>'+
-      '<h3>Credentials</h3>'+
-      '<div class=\'row\'>' +
-      '<div class=\'small-12 medium-7 columns\'>' +
-      '<label>App ID <a ng-click=\'showAppId()\'>({{ showId ? "Hide" : "Show" }})</a></label>'+
-      '<input ng-disabled=\'true\' type=\'text\' ng-model="appId"></input></li>'+
-      '</div>'+
-      '</div>'+
-      '<div class=\'row\'>' +
-      '<div class=\'small-12 medium-7 columns\'>' +
-      '<label>App Secret <a ng-click=\'showAppSecret()\'>({{ showSecret ? "Hide" : "Show" }})</a></label>'+
-      '<input ng-disabled=\'true\' type=\'text\' ng-model="appSecret"></input></li>'+
-      '</div>'+
-      '</div>'+
-      '<hr>'+
-      '<div class=\'row\'>' +
-      '<div class=\'small-12 medium-7 columns\'>' +
-      '<a href=\'/#/apps/{{app.id}}/edit\' class=\'button\'>Edit</a>'+
-      '</div>'+
-      '</div>'+
-      '</ul>'+
-      '</div>'
-
+    scope: {},
+    templateUrl: 'components/apps/_show.html'
   };
 
 }]);
 
-app.directive('editApp', ['App', '$routeParams', '$location', 'menu', 'showErrors', function(App, $routeParams, $location, menu, showErrors) {
+app.directive('editApp', ['App', '$routeParams', '$location', 'menu', 'showErrors', 'showToast', function(App, $routeParams, $location, menu, showErrors, showToast) {
 
   var link = function(scope) {
 
@@ -149,19 +106,19 @@ app.directive('editApp', ['App', '$routeParams', '$location', 'menu', 'showError
     var create = function() {
       App.create({app: scope.app}).$promise.then(function(results) {
         $location.path('/apps/' + results.id).search({n: true});
+        showToast('Application successfully created');
       }, function(err) {
         showErrors(err);
       });
     };
 
     var update = function() {
-      // App.update({id: $routeParams.id, app: scope.app}).$promise.then(function(results) {
-      //   $location.path('/apps/' + results.id).search({n: true});
-      // }, function(err) {
-      //   if ( err.data && err.data.message ) {
-      //     formatErrors(err.data.message);
-      //   }
-      // });
+      App.update({id: scope.app.id, app: scope.app}).$promise.then(function(results) {
+        $location.path('/apps/' + results.id).search({n: true});
+        showToast('Application successfully updated');
+      }, function(err) {
+        showErrors(err);
+      });
     };
 
     scope.save = function() {
