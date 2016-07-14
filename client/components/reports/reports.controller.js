d2df3e2a3af707e26f20754084aa9494a5efbdee
Simon Morley <simon@polkaspots.com>
2016-07-15 00:48:39 +0100

    Adds TZ guess to audits

--- client/components/reports/reports.controller.js
+++ client/components/reports/reports.controller.js
@@ -5,7 +5,4 @@ var app = angular.module('myApp.reports.controller', []);
 app.controller('ReportsCtrl', ['$scope', '$routeParams', 'Location', '$location', 'Box', '$filter', '$pusher', '$rootScope', '$route', 'menu', '$mdSidenav', '$q',
   function($scope, $routeParams, Location, $location, Box, $filter, $pusher, $rootScope, $route, menu, $mdSidenav, $q) {
 
-    $scope.location = {};
-
 }]);
-
