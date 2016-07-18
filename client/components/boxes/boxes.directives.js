3ceec8e92a73beccad25d3c14081019203b225b5
Simon Morley <simon@polkaspots.com>
2016-07-18 17:16:44 +0100

    Fixes #195 - heartbeats don't need to refresh the whole page. Awaits backend

--- client/components/boxes/boxes.directives.js
+++ client/components/boxes/boxes.directives.js
@@ -326,8 +326,8 @@ app.directive('showBox', ['Box', '$routeParams', 'Auth', '$pusher', '$location',
 
     var transferBox = function(id) {
       Box.update({
-        id: scope.box.slug, 
-        box: { 
+        id: scope.box.slug,
+        box: {
           transfer_to: id
         }
       }).$promise.then(function(results) {
@@ -375,7 +375,17 @@ app.directive('showBox', ['Box', '$routeParams', 'Auth', '$pusher', '$location',
     }
 
     var processNotification = function(data) {
+      if (data){
+        try{
+          data = JSON.parse(data);
+        }catch(e){
+          console.log(e);
+        }
+      }
       switch(data.type) {
+        case 'heartbeat':
+          heartbeat(data);
+          break;
         case 'speedtest':
           scope.box.speedtest_running = undefined;
           scope.box.allowed_job = true;
@@ -402,10 +412,18 @@ app.directive('showBox', ['Box', '$routeParams', 'Auth', '$pusher', '$location',
           }
           break;
         default:
-          // Replace this with the object as sent by pusher
-          // Needs to be replaced in the backend workers first
-          // init();
-          // Removed temporarily while we sort workers
+          console.log(data, 'Unknown Event');
+      }
+    };
+
+    var heartbeat = function(data) {
+      scope.box.last_heartbeat = data.last_heartbeat;
+      scope.box.state          = data.state;
+      scope.box.wan_ip         = data.wan_ip;
+      if (scope.box.state === 'offline') {
+        scope.box.allowed_job = false;
+      } else {
+        scope.box.allowed_job = true;
       }
     };
 
@@ -496,7 +514,7 @@ app.directive('showBox', ['Box', '$routeParams', 'Auth', '$pusher', '$location',
         }, function(error) {
           deferred.reject(error);
         });
-      } 
+      }
       return deferred.promise;
     };
 
