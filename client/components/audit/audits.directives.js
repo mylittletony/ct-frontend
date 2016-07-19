cb9b4c4b9c46436b1d3e8000d32ccc83a9065a36
Simon Morley <simon@polkaspots.com>
2016-07-19 11:18:46 +0100

    Adds interval for the email and guest search since it was causing a 422 on the back end

--- client/components/audit/audits.directives.js
+++ client/components/audit/audits.directives.js
@@ -296,6 +296,7 @@ app.directive('auditEmails', ['Email', '$routeParams', '$location', 'Client', '$
 
   var link = function( scope, element, attrs ) {
 
+    var interval        = 'day';
     scope.email         = $routeParams.email;
     scope.location_name = $routeParams.location_name;
 
@@ -391,7 +392,8 @@ app.directive('auditEmails', ['Email', '$routeParams', '$location', 'Client', '$
         location_name: scope.location_name,
         email: scope.email,
         start: scope.query.start,
-        end: scope.query.end
+        end: scope.query.end,
+        interval: interval
       };
       Email.get(params).$promise.then(function(results) {
         scope.emails      = results.emails;
@@ -933,7 +935,7 @@ app.directive('rangeFilter', ['$routeParams', '$mdDialog', '$location', 'gettext
       $scope.myDate = new Date();
       $scope.minDate = new Date(
         $scope.myDate.getFullYear(),
-        $scope.myDate.getMonth() - 2,
+        $scope.myDate.getMonth() - 3,
         $scope.myDate.getDate()
       );
       $scope.maxDate = new Date(
@@ -949,8 +951,8 @@ app.directive('rangeFilter', ['$routeParams', '$mdDialog', '$location', 'gettext
       $scope.search = function() {
         var hash = $location.search();
         hash.start = new Date($scope.startDate).getTime() / 1000;
-        hash.end   = new Date($scope.endDate).getTime() / 1000;
-
+        var end = new Date($scope.endDate);
+        hash.end = end.setDate(end.getDate() + 1) / 1000;
         if (hash.start >= hash.end) {
           $scope.error = gettextCatalog.getString('The start date must be less than the end date');
         } else {
