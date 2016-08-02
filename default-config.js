d9f3adb0c6aefbb263f5a1ac06008a4dfcddc618
Toni Marinova <a.marinova@gmail.com>
2016-08-02 16:19:30 +0300

    default-config.js changed the values of intercom and pusher

--- default-config.js
+++ default-config.js
@@ -21,8 +21,8 @@ module.exports = {
             AUTH_URL: 'http://id.mywifi.dev:8080',
             SLACK_TOKEN: '3540010629.12007999527',
             CHIMP_TOKEN: '531543883634',
-            INTERCOM: 'xxx',
-            PUSHER: 'xxx',
+            INTERCOM: 'z0kiwroa',
+            PUSHER: 'f5c774e098156e548079',
             DEBUG: true,
             COLOURS: '#009688 #FF5722 #03A9F4 #607D8B #F44336 #00BCD4'
         }
