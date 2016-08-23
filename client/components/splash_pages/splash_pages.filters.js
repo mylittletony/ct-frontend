fafcfcbd67193d6017ce8aca130a37b716c5e7aa
Toni Marinova <a.marinova@gmail.com>
2016-08-23 18:11:55 +0300

    translatable spalsh pages access type filter

--- client/components/splash_pages/splash_pages.filters.js
+++ client/components/splash_pages/splash_pages.filters.js
@@ -16,3 +16,23 @@ app.filter('periodicFilter', function() {
 
 });
 
+app.filter('translateAccessTypeName', ['gettextCatalog', function(gettextCatalog) {
+  return function(input) {
+    switch(input) {
+      case 1:
+        return gettextCatalog.getString('Password');
+      case 2:
+        return gettextCatalog.getString('Vouchers');
+      case 3:
+        return gettextCatalog.getString('Clickthrough');
+      case 7:
+        return gettextCatalog.getString('Social');
+      case 8:
+        return gettextCatalog.getString('Registration');
+      case 9:
+        return gettextCatalog.getString('QuickCodes');
+      default:
+        return gettextCatalog.getString('N/A');
+    }
+  };
+}]);
