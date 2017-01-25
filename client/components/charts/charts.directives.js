a71f18dcdebf07cdca794828744d092b7236029a
Zak Richardson <zak.r@cucumberwifi.io>
2017-01-25 16:23:18 +0000

    fixed test for merge

--- client/components/charts/charts.directives.js
+++ client/components/charts/charts.directives.js
@@ -125,7 +125,7 @@ app.directive('clientsChart', ['$timeout', '$rootScope', 'gettextCatalog', funct
           chart.draw(data, options);
         }
       }
-      // For the tests mainly, not sure why this has started causing a failure,, like above
+      // For the tests mainly, not sure why this has started causing a failure, like above
       if (window.google && window.google.visualization) {
         var date_formatter = new window.google.visualization.DateFormat({
           pattern: gettextCatalog.getString('MMM dd, yyyy hh:mm:ss a')
