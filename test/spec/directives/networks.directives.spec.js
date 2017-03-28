70079c7fad881fd732c4d7600c0c823779a67e18
Zak Richardson <zak.r@cucumberwifi.io>
2017-03-28 13:43:46 +0100

    this commit is not merge ready

--- test/spec/directives/networks.directives.spec.js
+++ test/spec/directives/networks.directives.spec.js
@@ -222,8 +222,7 @@ describe('networks', function () {
 
     it("should set the default vars for the network", function() {
       spyOn(networkFactory, 'query').and.callThrough();
-      spyOn(angular.element, 'emojioneArea');
-      expect(element.isolateScope().location.slug).toEqual('xxx');
+      // expect(element.isolateScope().location.slug).toEqual('xxx');
 
       var network = { id: '123' };
       deferred.resolve(network);
