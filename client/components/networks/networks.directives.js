a202972234955944d5b7ef8b0d71904ceb73a402
Zak Richardson <zak.r@cucumberwifi.io>
2017-04-03 10:55:31 +0100

    I accidentally ES5/6'd

--- client/components/networks/networks.directives.js
+++ client/components/networks/networks.directives.js
@@ -251,12 +251,12 @@ app.directive('ssid', function() {
       function emojiByteValidation(value) {
 
         function emojiCount(str){
-          const joiner = "\u{200D}";
-          const split = str.split(joiner);
-          let count = 0;
+          var joiner = "\u{200D}";
+          var split = str.split(joiner);
+          var count = 0;
 
           for(const s of split){
-            const num = Array.from(s.split(/[\ufe00-\ufe0f]/).join("")).length;
+            var num = Array.from(s.split(/[\ufe00-\ufe0f]/).join("")).length;
             count += num;
           }
           return count / split.length;
