ef024a9ce8ce4ae01898fbdf885b971ca2c32157
Zak <zak@polkaspots.com>
2017-05-05 13:32:12 +0100

    directive

--- client/components/locations/locations.directives.js
+++ client/components/locations/locations.directives.js
@@ -2315,3 +2315,9 @@ app.directive('dashInventory', ['Report', 'Auth', function(Report, Auth) {
   };
 
 }]);
+
+app.directive('homeStatCards', function() {
+  return {
+    templateUrl: 'components/locations/show/_home_stat_cards.html'
+  };
+});
