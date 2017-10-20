0a62e95bbc78402fa28b93a1ab2e3a2b873dee40
Ewan <ewan.s@cucumberwifi.io>
2017-10-20 13:32:37 +0100

    do not redirect to brand url after login - only custom domain

--- client/app/main/main.controllers.js
+++ client/app/main/main.controllers.js
@@ -264,14 +264,14 @@ app.controller('MainCtrl', ['$rootScope', '$scope', '$localStorage', '$window', 
       if ((cname === null || cname === '') && event.data.url !== 'default') {
         var sub   = locationHelper.subdomain();
         var host  = locationHelper.domain();
-        if (event.data.url && (sub !== event.data.url)) {
-          var newUrl = locationHelper.reconstructed(event.data.url);
-          var reconstructed = newUrl + '/#/';
-          $cookies.put('event', JSON.stringify(event), {'domain': host});
-          window.location = reconstructed;
-        } else {
+        // if (event.data.url && (sub !== event.data.url)) {
+          // var newUrl = locationHelper.reconstructed(event.data.url);
+          // var reconstructed = newUrl + '/#/';
+          // $cookies.put('event', JSON.stringify(event), {'domain': host});
+          // window.location = reconstructed;
+        // } else {
           doLogin(event);
-        }
+        // }
       } else {
         doLogin(event);
       }
