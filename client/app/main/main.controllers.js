9b0e8dd0f9f99c302bec1e32ae14c32cbf9335f0
Ewan <ewan.s@cucumberwifi.io>
2017-11-07 16:50:48 +0000

    hide intercom for reseller users

--- client/app/main/main.controllers.js
+++ client/app/main/main.controllers.js
@@ -290,35 +290,34 @@ app.controller('MainCtrl', ['$rootScope', '$scope', '$localStorage', '$window', 
     }
 
     $scope.$on('intercom', function(args,event) {
-      if (Auth.currentUser() && (Auth.currentUser().chat_enabled !== false && Auth.currentUser().user_hash !== undefined)) {
+      if (Auth.currentUser() && Auth.currentUser().user_hash !== undefined) {
         vm.menu.Intercom = true;
-        var intercom_id;
-        if ($scope.brandName.reseller === true && Auth.currentUser().reseller !== true) {
-          intercom_id = $scope.brandName.intercom_id;
+        window.intercomSettings = {
+            app_id: INTERCOM,
+            user_id: Auth.currentUser().accountId,
+            reseller: Auth.currentUser().reseller,
+            email: Auth.currentUser().email,
+            name: Auth.currentUser().username,
+            created_at: Auth.currentUser().created_at,
+            user_hash: Auth.currentUser().user_hash,
+            brand_name: Auth.currentUser().url,
+            cname: Auth.currentUser().cname,
+            sense_active: Auth.currentUser().sense_active,
+            plan_name: Auth.currentUser().plan_name,
+            paid_plan: Auth.currentUser().paid_plan,
+            locs: Auth.currentUser().locs,
+            version: '2',
+            hide_default_launcher: !Auth.currentUser().chat_enabled || false
+        };
+      }
+
+      // check for customers from reseller brands who aren't resellers
+      if ($scope.brandName && $scope.brandName.reseller === true && Auth.currentUser().reseller !== true) {
+        // check if there is a special brand intercom id, otherwise just hide it
+        if ($scope.brandName.intercom_id) {
+          window.intercomSettings.app_id = $scope.brandName.intercom_id
         } else {
-          intercom_id = INTERCOM
-        }
-        if (intercom_id !== undefined) {
-          var settings = {
-              app_id: intercom_id,
-              user_id: Auth.currentUser().accountId,
-              reseller: Auth.currentUser().reseller,
-              email: Auth.currentUser().email,
-              name: Auth.currentUser().username,
-              created_at: Auth.currentUser().created_at,
-              user_hash: Auth.currentUser().user_hash,
-              brand_name: Auth.currentUser().url,
-              cname: Auth.currentUser().cname,
-              sense_active: Auth.currentUser().sense_active,
-              plan_name: Auth.currentUser().plan_name,
-              paid_plan: Auth.currentUser().paid_plan,
-              locs: Auth.currentUser().locs,
-              version: '2'
-          };
-          settings.widget = {
-            activator: '#intercom'
-          };
-          window.Intercom('boot', settings);
+          window.intercomSettings.hide_default_launcher = true
         }
       }
     });
