55567a3bd1e8723be67989d6fb5b070992418a22
Simon Morley <simon@polkaspots.com>
2017-12-23 13:54:24 +0000

    Updates

--- client/components/boxes/boxes.directives.js
+++ client/components/boxes/boxes.directives.js
@@ -231,7 +231,7 @@ app.directive('showBox', ['Box', '$routeParams', 'Auth', '$pusher', '$location',
         console.log(errors);
         scope.box.state = 'failed';
         scope.resetting = undefined;
-        showErrors(errors)
+        showErrors(errors);
       });
     };
 
@@ -466,6 +466,32 @@ app.directive('showBox', ['Box', '$routeParams', 'Auth', '$pusher', '$location',
       menu.toggleSelectSection(section);
     };
 
+    var connectionStatus = function() {
+      switch (scope.box.connection_status) {
+        case '9':
+          scope.box.connection_status_formatted = 'OK';
+          break;
+        case '9.1':
+          scope.box.connection_status_formatted = 'NTP failed';
+          break;
+        case '1':
+          scope.box.connection_status_formatted = 'HTTP and DNS check failed';
+          break;
+        case '4':
+          scope.box.connection_status_formatted = 'HTTP check failed';
+          break;
+        case '0':
+          scope.box.connection_status_formatted = 'Communication error';
+          break;
+        case '6':
+          scope.box.connection_status_formatted = 'DNS check failed';
+          break;
+        default:
+          scope.box.connection_status_formatted = `Misc. Problem (` + scope.box.connection_status + `)`;
+          break;
+      }
+    };
+
     var init = function() {
       var deferred = $q.defer();
       Box.get({id: $routeParams.box_id, metadata: true}).$promise.then(function(box) {
@@ -476,6 +502,7 @@ app.directive('showBox', ['Box', '$routeParams', 'Auth', '$pusher', '$location',
           version: box.v
         };
         scope.loading = undefined;
+        connectionStatus();
         poll();
         deferred.resolve();
       }, function() {
