8f6ab785fa88bf9cf687e7b37616ef7f792939ca
Ewan <ewan.s@cucumberwifi.io>
2018-01-23 15:35:06 +0000

    remove bulk message junk

--- client/components/directives/bulk_message_activity.directives.js
+++ client/components/directives/bulk_message_activity.directives.js
@@ -6,46 +6,6 @@ app.directive('bulkMessageActivity', ['$routeParams', 'BulkMessageActivity', 'Pe
 
   var link = function( scope, element, attrs ) {
 
-    // var person = {};
-    //
-    // var fetchPerson = function() {
-    //   People.query({location_id: $routeParams.id, id: $routeParams.person_id}).$promise.then(function(res) {
-    //     person = res;
-    //     fetchMessages();
-    //   }, function(err) {
-    //     console.log(err);
-    //   });
-    // };
-    //
-    // var fetchMessages = function() {
-    //
-    //   BulkMessage.index({}, {
-    //     person_id:    person.id || $routeParams.person_id,
-    //     location_id:  $routeParams.id,
-    //     start:        $routeParams.start,
-    //     end:          $routeParams.end
-    //   }).$promise.then(function(results) {
-    //     scope.loading = undefined;
-    //     scope.messages = results.messages;
-    //   });
-    // };
-    //
-    // scope.query = function(person_id) {
-    //   var hash            = {};
-    //   hash.person_id      = person_id;
-    //   hash.per            = $routeParams.per || 100;
-    //   hash.start          = $routeParams.start;
-    //   hash.end            = $routeParams.end;
-    //   $location.search(hash);
-    //   fetchMessages();
-    // };
-    //
-    // if ($routeParams.person_slug) {
-    //   fetchPerson();
-    // } else {
-    //   fetchMessages();
-    // }
-
     BulkMessageActivity.index({}, {
       location_id:  $routeParams.id,
       start:        $routeParams.start,
