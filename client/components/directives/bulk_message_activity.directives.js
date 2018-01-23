61507e516a9019ad43a84a909df4303042822470
Ewan <ewan.s@cucumberwifi.io>
2018-01-23 17:02:31 +0000

    activity by person

--- client/components/directives/bulk_message_activity.directives.js
+++ client/components/directives/bulk_message_activity.directives.js
@@ -9,17 +9,36 @@ app.directive('bulkMessageActivity', ['$routeParams', 'BulkMessageActivity', 'Pe
     scope.location = {slug: $routeParams.id};
     scope.person = {slug: $routeParams.person_slug};
 
-    BulkMessageActivity.index({}, {
-      location_id:  scope.location.slug,
-      start:        $routeParams.start,
-      end:          $routeParams.end,
-      message_id:   $routeParams.message_id,
-      person_id:    $routeParams.person_slug
-    }).$promise.then(function(results) {
-      scope.loading = undefined;
-      scope.activity = results.activity;
-    });
-    scope.loading = undefined;
+    var fetchMessageActivity = function() {
+      BulkMessageActivity.index({}, {
+        location_id:  scope.location.slug,
+        start:        $routeParams.start,
+        end:          $routeParams.end,
+        message_id:   $routeParams.message_id,
+        person_id:    scope.person.id
+      }).$promise.then(function(results) {
+        scope.loading = undefined;
+        scope.activity = results.activity;
+      });
+    }
+
+    var fetchPerson = function() {
+      People.query({}, {
+          location_id: scope.location.slug,
+          id: $routeParams.person_slug
+        }).$promise.then(function(res) {
+        scope.person = res;
+        fetchMessageActivity();
+      }, function(err) {
+        console.log(err);
+      });
+    };
+
+    if ($routeParams.person_slug) {
+      fetchPerson();
+    } else {
+      fetchMessageActivity();
+    }
 
   };
 
