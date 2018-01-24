df078b0ffd9d1897203e1771ff2e100285c385ea
Ewan <ewan.s@cucumberwifi.io>
2018-01-24 16:37:51 +0000

    dont be silly

--- client/components/services/metrics.services.js
+++ client/components/services/metrics.services.js
@@ -9,7 +9,7 @@ app.factory('Metric', ['$resource', '$localStorage', 'API_END_POINT',
       token = $localStorage.user.api_token;
     }
     if (API_END_POINT === 'http://mywifi.test:8080/api/v1') {
-      API_END_POINT = 'http://dashboard.ctapp:8000/api/v1'
+      API_END_POINT = 'http://dashboard.ctapp:8000/api/v1';
     }
     return $resource(API_END_POINT + '/metrics',
       {
