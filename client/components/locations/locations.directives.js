381d9b736741b610bd0795d47c7ddd0ae367c803
Simon Morley <simon@polkaspots.com>
2018-02-01 17:01:19 +0000

    Updates styling and plans

--- client/components/locations/locations.directives.js
+++ client/components/locations/locations.directives.js
@@ -1379,9 +1379,10 @@ app.directive('getWithThePlan', ['Location', '$routeParams', '$location', 'Subsc
     var channel;
 
     function DoucheController($scope, plans) {
+      console.log(plans)
       $scope.selectedIndex = 0;
       $scope.plans = plans;
-      $scope.plan = plans[0];
+      $scope.plan  = plans[1];
       $scope.plan_id = $scope.plan.slug;
 
       $scope.setPlan = function(plan_id) {
