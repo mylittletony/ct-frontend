1c7c71afc0b36d6a89c7287719509771bd45230f
Ewan <ewan.s@cucumberwifi.io>
2018-02-01 12:19:03 +0000

    prof pic

--- client/components/people/people.directives.js
+++ client/components/people/people.directives.js
@@ -129,9 +129,20 @@ app.directive('displayPerson', ['People', 'Location', '$routeParams', '$location
       });
     };
 
+    var setProfilePhoto = function(social) {
+      if (social[0].facebook_id) {
+        scope.person.profile_photo = 'https://graph.facebook.com/' + social[0].facebook_id + '/picture?type=large';
+      } else {
+        scope.person.profile_photo = 'https://s3-eu-west-1.amazonaws.com/mimo-labs/images/mimo-logo.svg';
+      }
+    };
+
     var getPerson = function() {
       People.query({location_id: scope.location.slug, id: $routeParams.person_id}).$promise.then(function(res) {
         scope.person = res;
+        if (res.social) {
+          setProfilePhoto(res.social);
+        }
         scope.loading  = undefined;
       }, function(err) {
         scope.loading  = undefined;
