bd0e642da9ad769091cf6e93ef4e738c1e33842f
Simon Morley <simon@polkaspots.com>
2018-02-07 16:59:27 +0000

    client id

--- client/components/registrations/registrations.services.js
+++ client/components/registrations/registrations.services.js
@@ -23,7 +23,8 @@ app.factory('Holding', ['$resource', 'API_END_POINT',
       {
         id: '@id',
         holding_account: '@holding_account',
-        brand: 'mimo'
+        brand: 'mimo',
+        client_id: '@client_id'
       },
       {
       create: {
