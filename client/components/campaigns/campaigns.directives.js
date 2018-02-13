14e66447e7cd19182abd51cd04bc4537701a2cf0
ZakMooney <zak@polkaspots.com>
2018-02-13 16:25:10 +0000

    added and styled

--- client/components/campaigns/campaigns.directives.js
+++ client/components/campaigns/campaigns.directives.js
@@ -128,6 +128,22 @@ app.directive('editCampaign', ['Campaign', 'Location', 'Integration', 'Auth', '$
 
     scope.states = ['draft', 'live'];
 
+    scope.tinymceOptions = {
+      selector: 'textarea',
+      height: 200,
+      menubar: false,
+      plugins: [
+        'advlist autolink lists link image charmap print preview anchor textcolor',
+        'searchreplace visualblocks code fullscreen',
+        'insertdatetime media table contextmenu paste code wordcount'
+      ],
+      toolbar: 'insert | undo redo |  formatselect | bold italic backcolor  | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | removeformat | code',
+      content_css: [
+        '//fonts.googleapis.com/css?family=Lato:300,300i,400,400i',
+        '//www.tinymce.com/css/codepen.min.css'
+      ]
+    };
+
     scope.addRule = function() {
       if (!scope.campaign.holding_predicates) {
         scope.campaign.holding_predicates = [];
